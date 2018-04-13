4430fdbf241b999b51cfbbd20bf967d1ee0d7462
Pushan-Gore <pushan.gore@gmail.com>
2018-04-13 16:13:48 +0530

    Added training set features to the training code

--- train.py
+++ train.py
@@ -43,13 +43,41 @@ def train_set():
     trainFeatures = posFeatures[:] + negFeatures[:]
 
     #trains a Naive Bayes Classifier
-    print trainFeatures
     classifier = NaiveBayesClassifier.train(trainFeatures)
-
     #Dump the classifier to a pickle file
     naive_bayes_classifier_file = open(NAIVEBAYES_CLASS_FILE, 'wb')
     pickle.dump(classifier, naive_bayes_classifier_file)
     naive_bayes_classifier_file.close()
+
+    # Retrain the feature set for analysis:
+    # Determine the precision and recall
+    trainFeatures = []
+    testFeatures = []
+    posCutoff = int(math.floor(len(posFeatures)*3/4))
+    negCutoff = int(math.floor(len(negFeatures)*3/4))
+    trainFeatures = posFeatures[:posCutoff] + negFeatures[:negCutoff]
+    testFeatures = posFeatures[posCutoff:] + negFeatures[negCutoff:]
+
+    #initiates referenceSets and testSets
+    referenceSets = collections.defaultdict(set)
+    testSets = collections.defaultdict(set)
+
+    # Train classifier again
+    test_classifier = NaiveBayesClassifier.train(trainFeatures)
+
+    #puts correctly labeled sentences in referenceSets and the predictively labeled version in testsets
+    for i, (features, label) in enumerate(testFeatures):
+        referenceSets[label].add(i)
+	predicted = test_classifier.classify(features)
+	testSets[predicted].add(i)
+
+    #prints metrics to show how well the feature selection did
+    print 'train on %d instances, test on %d instances' % (len(trainFeatures), len(testFeatures))
+    print 'accuracy:', nltk.classify.util.accuracy(test_classifier, testFeatures)
+    print 'pos precision:', precision(referenceSets['pos'], testSets['pos'])
+    print 'pos recall:', recall(referenceSets['pos'], testSets['pos'])
+    print 'neg precision:', precision(referenceSets['neg'], testSets['neg'])
+    print 'neg recall:', recall(referenceSets['neg'], testSets['neg'])
 
     #prints metrics to show how well the feature selection did
     classifier.show_most_informative_features(10)
